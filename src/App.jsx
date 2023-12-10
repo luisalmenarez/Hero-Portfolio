66d9763ac519ce26ff21f15f75bca20b20f375a9
Luis Almenarez <luisalmenarez0428@gmail.com>
2023-12-10 03:02:49 -0500

    Update animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 import { Content } from "./Components/Content";
 import { Header } from "./Components/Header";
 
@@ -10,14 +11,26 @@ export const App = () => {
           alt="Image"
           className="h-full object-cover grayscale"
         />
-        <span className="absolute w-40 h-40 bg-pink-500/80 rounded-full -top-20 -left-20 shadow-md shadow-pink-500"></span>
+        <motion.span
+          initial={{ opacity: 0, x: -50 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ delay: 0.8, duration: 0.5 }}
+          className="absolute w-40 h-40 bg-pink-500/80 rounded-full -top-20 -left-20 shadow-md shadow-pink-500"></motion.span>
       </section>
       <section className="lg:col-span-8 relative overflow-hidden">
         <Header />
         <hr />
         <Content />
-        <span className="absolute w-40 h-40 bg-pink-500/80 rounded-full -bottom-32 -right-10"></span>
-        <span className="absolute w-60 h-60 bg-pink-500/80 rounded-full top-10 -right-48 shadow-2xl shadow-pink-500"></span>
+        <motion.span
+          initial={{ opacity: 0, x: 50 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ delay: 1, duration: 0.5 }}
+          className="absolute w-40 h-40 bg-pink-500/80 rounded-full -bottom-32 -right-10"></motion.span>
+        <motion.span
+          initial={{ opacity: 0, x: -50 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ delay: 1.3, duration: 0.5 }}
+          className="absolute w-60 h-60 bg-pink-500/80 rounded-full top-10 -right-48 shadow-2xl shadow-pink-500"></motion.span>{" "}
       </section>
     </main>
   );
